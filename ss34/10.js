bfbfa3a8fb9ca9469c74c4a74b4c54b96cd02ee4
HuynhTamAnh <huynhtamanh2000@gmail.com>
2024-03-13 08:18:52 +0700

    1203

---
+++ ss34/10.js
@@ -0,0 +1,5 @@
+var divElement = document.getElementById("example");
+
+console.log(divElement.innerText); // Kết quả: This is inner text.
+console.log(divElement.textContent); // Kết quả: This is inner text.
+console.log(divElement.innerHTML); // Kết quả: This is <strong>inner</strong> text.
